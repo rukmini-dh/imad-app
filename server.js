731fc5b97a35d5d5db232614a762d42497f4d037
rukmini-dh <31001106+rukmini-dh@users.noreply.github.com>
2017-08-25 22:21:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/chitra',function(rew,req){res.sendFile("hurray")});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
