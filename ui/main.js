3c18035c76e1650bb4237eeaf415204719b34dcd
rukmini-dh <31001106+rukmini-dh@users.noreply.github.com>
2017-09-03 23:16:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById("new-value");
 element.innerHTML = 'new value';
-var img = document.getElementbyId("img");
+var img = document.getElementById("img");
 img.onclick= function(){
     img.style.marginLeft = '100px';
     
